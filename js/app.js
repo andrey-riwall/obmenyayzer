fbfed00eef51ca1a09a421ed6559bba9c0ba3c56
Андрей Мирский <andrey.riwall@gmail.com>
2021-06-30 19:39:18 +0300

    2

--- js/app.js
+++ js/app.js
@@ -5,27 +5,27 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log(document.querySelector('#wheel2'))
   }
   
-  setTimeout(end2, 3600);
+  setTimeout(end2, 3500);
   
   
   function end3() {
     document.querySelector('#wheel3').classList.add('end3');
   }
   
-  setTimeout(end3, 6400);
+  setTimeout(end3, 6300);
   
   
   function end4() {
     document.querySelector('#wheel4').classList.add('end4');
   }
   
-  setTimeout(end4, 9600);
+  setTimeout(end4, 9500);
   
   
   function end5() {
     document.querySelector('#wheel5').classList.add('end5');
   }
   
-  setTimeout(end5, 12800);  
+  setTimeout(end5, 12700);  
 
 })
